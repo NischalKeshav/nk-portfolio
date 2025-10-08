8ced60605e837fbe252ee0a4b3ac89d36ef00dd7
Nischal Keshav <nischalkeshav.official@gmail.com>
2025-10-08 09:26:00 -0500

    new updates

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import "./glass.css";
+import "./globals.css";
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-background text-foreground font-sans">
+      <p className="bg-blue-500 w-1/2">Hello world</p>
+      
+    </div>
+  );
+}
